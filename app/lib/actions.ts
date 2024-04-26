4700bf98b5fecef0f5d3030dc6e11a8c8664d88e
a20diti <aditi.singh@kindasolutions.com>
2024-04-26 11:29:46 +0530

    changes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
